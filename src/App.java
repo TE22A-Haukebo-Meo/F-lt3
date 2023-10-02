9becac4263e21927c3a4ab980017811a95ab355f
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-02 09:20:07 +0200

    loop moment

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String [] sui = new String [5];
-        sui [0] = "Nice guy";
-        sui [1] = "Nice try";
-        sui [2] = "Havet sträcker sig långt ögat kan se";
-        sui [3] = "Fboy";
-        sui [4] = "Fbye";
+        String [] sui = new String [6];
+        sui [0] = "Gå in i huset";
+        sui [1] = "Gå ut ur huset";
+        sui [2] = "Gå in i huset";
+        sui [3] = "Gå ur ur huset";
+        sui [4] = "En läskig demon åt din lilltå";
+        sui[5] = "Du dog.";
         int steg = 0;
-        
-        
 
+        for (steg =0; steg <6; steg++){
+            System.out.println(sui[steg]);
+        }
 }
