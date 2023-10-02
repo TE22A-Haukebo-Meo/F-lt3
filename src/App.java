fd8ae532aef0aa0bcd680b8a7886281814d8dce2
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-02 09:06:19 +0200

    Börjat

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String [] sui = new String [5];
+        sui [0] = "Nice guy";
+        sui [1] = "Nice try";
+        sui [2] = "Havet sträcker sig långt ögat kan se";
+        sui [3] = "Fboy";
+        sui [4] = "Fbye";
+        int steg = 0;
+        
+        
+
+}
